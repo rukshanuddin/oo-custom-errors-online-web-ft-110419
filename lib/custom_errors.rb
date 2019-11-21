0642bf20bdc24880db59666c8e648e04097aec3a
Rukshan Uddin <rukshan.uddin@gmail.com>
2019-11-21 01:52:12 +0000

    Automatically backed up by Learn

--- lib/custom_errors.rb
+++ lib/custom_errors.rb
@@ -16,8 +16,11 @@ class Person
   end
 
   class PartnerError < StandardError
-  end
   
+    def message
+      "you must give the get_married method an argument of an instance of the person class!"
+    end
+  end
 end
 
 beyonce = Person.new("Beyonce")
